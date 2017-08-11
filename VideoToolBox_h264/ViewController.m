454c6fc4c26d16b82b7436caef8c29e771d387e3
liyanyan <liyanyan@3mang.com>
2017-08-11 16:27:54 +0800

    编码

--- VideoToolBox_h264/ViewController.m
+++ VideoToolBox_h264/ViewController.m
@@ -18,11 +18,11 @@
 
 @interface ViewController ()<AVCaptureVideoDataOutputSampleBufferDelegate>
 
-@property(nonatomic,strong)UILabel *cLabel;
-@property(nonatomic,strong)AVCaptureSession *cCapturesession;//捕捉会话，输入输出的设备处理,设备连接
-@property(nonatomic,strong)AVCaptureDeviceInput *cCaptureDeviceInput;//捕捉输入,从摄像头输入数据
-@property(nonatomic,strong)AVCaptureVideoDataOutput *cCaptureDataOutput;//捕捉的数据输出到哪里
-@property(nonatomic,strong)AVCaptureVideoPreviewLayer *cPreviewLayer;//预览图层
+@property(nonatomic,strong)UILabel *lvyLabel;
+@property(nonatomic,strong)AVCaptureSession *lvyCapturesession;//捕捉会话，输入输出的设备处理,设备连接
+@property(nonatomic,strong)AVCaptureDeviceInput *lvyCaptureDeviceInput;//捕捉输入,从摄像头输入数据
+@property(nonatomic,strong)AVCaptureVideoDataOutput *lvyCaptureDataOutput;//捕捉的数据输出到哪里
+@property(nonatomic,strong)AVCaptureVideoPreviewLayer *lvyPreviewLayer;//预览图层
 
 @end
 
@@ -41,12 +41,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     //基础UI实现
-    _cLabel = [[UILabel alloc]initWithFrame:CGRectMake(20, 20, 200, 100)];
-    _cLabel.text = @"cc课堂之H.264硬编码";
-    _cLabel.textColor = [UIColor redColor];
-    [self.view addSubview:_cLabel];
+    _lvyLabel = [[UILabel alloc]initWithFrame:CGRectMake(20, 20, 200, 50)];
+    _lvyLabel.text = @"H.264硬编码";
+    _lvyLabel.textColor = [UIColor redColor];
+    [self.view addSubview:_lvyLabel];
     
-    UIButton *cButton = [[UIButton alloc]initWithFrame:CGRectMake(200, 20, 100, 100)];
+    UIButton *cButton = [[UIButton alloc]initWithFrame:CGRectMake(200, 20, 100, 50)];
     [cButton setTitle:@"play" forState:UIControlStateNormal];
     [cButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
     [cButton setBackgroundColor:[UIColor orangeColor]];
@@ -56,7 +56,7 @@
 -(void)buttonClick:(UIButton *)button
 {
     
-    if (!_cCapturesession || !_cCapturesession.isRunning ) {//开始捕捉
+    if (!_lvyCapturesession || !_lvyCapturesession.isRunning ) {//开始捕捉
         
         [button setTitle:@"Stop" forState:UIControlStateNormal];
         [self startCapture];
@@ -73,64 +73,66 @@
 - (void)startCapture
 {
     
-    self.cCapturesession = [[AVCaptureSession alloc]init];//给捕捉对象开辟空间
+    self.lvyCapturesession = [[AVCaptureSession alloc]init];//给捕捉对象开辟空间
     
-    self.cCapturesession.sessionPreset = AVCaptureSessionPreset640x480;//设置分辨率
+    self.lvyCapturesession.sessionPreset = AVCaptureSessionPreset640x480;//设置分辨率
     
     cCaptureQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);//捕捉队列
     cEncodeQueue  = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);//编码队列
     
     AVCaptureDevice *inputCamera = nil;//捕捉的设备
     NSArray *devices = [AVCaptureDevice devicesWithMediaType:AVMediaTypeVideo];//拿到输入设备（前置后置摄像头）
+    
     for (AVCaptureDevice *device in devices) {
         if ([device position] == AVCaptureDevicePositionBack) {//根据position判断是前置摄像头还是后置摄像头
             inputCamera = device;
         }
     }
     
-    self.cCaptureDeviceInput = [[AVCaptureDeviceInput alloc]initWithDevice:inputCamera error:nil];//AVFoundation不能直接识别AVCaptureDevice，需要包装成AVCaptureDeviceInput
+    self.lvyCaptureDeviceInput = [[AVCaptureDeviceInput alloc]initWithDevice:inputCamera error:nil];//AVFoundation不能直接识别AVCaptureDevice，需要包装成AVCaptureDeviceInput
     
     //判断是否能加入到AVCaptureSession里面去，可以的话就加入
-    if ([self.cCapturesession canAddInput:self.cCaptureDeviceInput]) {
+    if ([self.lvyCapturesession canAddInput:self.lvyCaptureDeviceInput]) {
         
-        [self.cCapturesession addInput:self.cCaptureDeviceInput];
+        [self.lvyCapturesession addInput:self.lvyCaptureDeviceInput];
         
     }
     
-    self.cCaptureDataOutput = [[AVCaptureVideoDataOutput alloc]init];
+    self.lvyCaptureDataOutput = [[AVCaptureVideoDataOutput alloc]init];
     
-    [self.cCaptureDataOutput setAlwaysDiscardsLateVideoFrames:NO];
+    [self.lvyCaptureDataOutput setAlwaysDiscardsLateVideoFrames:NO];
     
     //设置输入格式
-    [self.cCaptureDataOutput setVideoSettings:[NSDictionary dictionaryWithObject:[NSNumber numberWithInt:kCVPixelFormatType_420YpCbCr8BiPlanarFullRange] forKey:(id)kCVPixelBufferPixelFormatTypeKey]];
+    [self.lvyCaptureDataOutput setVideoSettings:[NSDictionary dictionaryWithObject:[NSNumber numberWithInt:kCVPixelFormatType_420YpCbCr8BiPlanarFullRange] forKey:(id)kCVPixelBufferPixelFormatTypeKey]];
     
     //设置输出队列
-    [self.cCaptureDataOutput setSampleBufferDelegate:self queue:cCaptureQueue];
+    [self.lvyCaptureDataOutput setSampleBufferDelegate:self queue:cCaptureQueue];
     
     //添加到输出队列
-    if ([self.cCapturesession canAddOutput:self.cCaptureDataOutput]) {
+    if ([self.lvyCapturesession canAddOutput:self.lvyCaptureDataOutput]) {
         
-        [self.cCapturesession addOutput:self.cCaptureDataOutput];
+        [self.lvyCapturesession addOutput:self.lvyCaptureDataOutput];
     }
     
     //建立连接
-    AVCaptureConnection *connection = [self.cCaptureDataOutput connectionWithMediaType:AVMediaTypeVideo];
+    AVCaptureConnection *connection = [self.lvyCaptureDataOutput connectionWithMediaType:AVMediaTypeVideo];
     
     //设置方向
     [connection setVideoOrientation:AVCaptureVideoOrientationPortrait];
     
     //设置预览图层
-    self.cPreviewLayer = [[AVCaptureVideoPreviewLayer alloc]initWithSession:self.cCapturesession];
+    self.lvyPreviewLayer = [[AVCaptureVideoPreviewLayer alloc]initWithSession:self.lvyCapturesession];
     
-    [self.cPreviewLayer setVideoGravity:AVLayerVideoGravityResizeAspect];
+    [self.lvyPreviewLayer setVideoGravity:AVLayerVideoGravityResizeAspect];
     
-    [self.cPreviewLayer setFrame:self.view.bounds];
+    [self.lvyPreviewLayer setFrame:self.view.bounds];
     
-    [self.view.layer addSublayer:self.cPreviewLayer];
+    [self.view.layer addSublayer:self.lvyPreviewLayer];
     
     
     //文件写入
-    NSString *filePath = [NSHomeDirectory()stringByAppendingPathComponent:@"/Documents/cc_video.h264"];
+    //文件路径
+    NSString *filePath = [NSHomeDirectory()stringByAppendingPathComponent:@"/Documents/video.h264"];
     
     [[NSFileManager defaultManager] removeItemAtPath:filePath error:nil];
     
@@ -144,7 +146,8 @@
     }
     
     NSLog(@"filePaht = %@",filePath);
-    //一点点写入文件
+    
+    //编码数据并不是一次性写入的，是一点点写入文件
     fileHandele = [NSFileHandle fileHandleForWritingAtPath:filePath];
     
     
@@ -152,7 +155,7 @@
     [self initVideoToolBox];
     
     //开始捕捉
-    [self.cCapturesession startRunning];
+    [self.lvyCapturesession startRunning];
     
 }
 
@@ -161,9 +164,9 @@
 - (void)stopCapture
 {
     
-    [self.cCapturesession stopRunning];
+    [self.lvyCapturesession stopRunning];
     
-    [self.cPreviewLayer removeFromSuperlayer];
+    [self.lvyPreviewLayer removeFromSuperlayer];
     
     [self endVideoToolBox];
     
@@ -206,10 +209,12 @@
          参数9：回调参考值
          参数10：编码回话变量
          */
-        OSStatus status = VTCompressionSessionCreate(NULL, width, height, kCMVideoCodecType_H264, NULL, NULL, NULL, didCompressH264, (__bridge void *)(self), &cEncodeingSession);
+        //        OSStatus status =
+        VTCompressionSessionCreate(NULL, width, height, kCMVideoCodecType_H264, NULL, NULL, NULL, didCompressH264, (__bridge void *)(self), &cEncodeingSession);
         
         //设置实时编码
         VTSessionSetProperty(cEncodeingSession, kVTCompressionPropertyKey_RealTime, kCFBooleanTrue);
+        //设置实时的key
         VTSessionSetProperty(cEncodeingSession, kVTCompressionPropertyKey_ProfileLevel, kVTProfileLevel_H264_Baseline_AutoLevel);
         
         //设置关键帧（GOP)  间隔
@@ -224,7 +229,7 @@
         
         VTSessionSetProperty(cEncodeingSession, kVTCompressionPropertyKey_ExpectedFrameRate, fpsRef);
         
-        //码率  上线  最大期望值
+        //码率  上线  最大期望值   码率可以动态调整
         int bigRate = width * height *3 *4 *8;
         
         CFNumberRef bitRateRef =CFNumberCreate(kCFAllocatorDefault, kCFNumberSInt32Type, &bigRate);
@@ -273,10 +278,11 @@
 /*
  1.H264编码完成后，回调
  2.将编码成功的CMSampleBufferRef 转为 H264码流
- 3.解析SPS & PPS 组装码  nalu
+ 3.解析SPS & PPS 组装码  nalu流数据
  */
 void didCompressH264(void *outputCallbackRefCon, void *sourceFrameRefCon, OSStatus status, VTEncodeInfoFlags infoFlags, CMSampleBufferRef sampleBuffer)
 {
+    
     if(status != 0){
         return;
     }
@@ -287,9 +293,10 @@ void didCompressH264(void *outputCallbackRefCon, void *sourceFrameRefCon, OSStat
     
     ViewController *encoder = (__bridge ViewController *)outputCallbackRefCon;
     
-    bool keyFrame = !CFDictionaryContainsKey((CFArrayGetValueAtIndex(CMSampleBufferGetSampleAttachmentsArray(sampleBuffer, true), 0)), kCMSampleAttachmentKey_NotSync);//判断关键帧
-    
+    bool keyFrame = !CFDictionaryContainsKey((CFArrayGetValueAtIndex(CMSampleBufferGetSampleAttachmentsArray(sampleBuffer, true), 0)), kCMSampleAttachmentKey_NotSync); //判断是否是关键帧
+    // 解析出参数集SPS和PPS，加上开始码后组装成NALU。提取出视频数据，将长度码转换成开始码，组长成NALU。将NALU发送出去。
     if (keyFrame) {
+        // CMVideoFormatDescription：图像存储方式，编解码器等格式描述
         CMFormatDescriptionRef format = CMSampleBufferGetFormatDescription(sampleBuffer);
         
         //sps
@@ -312,7 +319,7 @@ void didCompressH264(void *outputCallbackRefCon, void *sourceFrameRefCon, OSStat
             }
         }
     }
-    
+    // 编码后的图像，以CMBlockBuffe方式存储
     CMBlockBufferRef dataBuffer  = CMSampleBufferGetDataBuffer(sampleBuffer);
     
     size_t length,totalLength;
@@ -324,33 +331,36 @@ void didCompressH264(void *outputCallbackRefCon, void *sourceFrameRefCon, OSStat
     if (statusCodeRet == noErr) {
         
         size_t bufferOffSet = 0;
+        // 返回的nalu数据前四个字节不是0001的startcode，而是大端模式的帧长度length
         static const int AVVCHeaderLength = 4;
-        
+        // 循环获取nalu数据
         while (bufferOffSet <totalLength - AVVCHeaderLength) {
-            uint32_t NALUnitLength = 0;
-            memcpy(&NALUnitLength, dataPointer + bufferOffSet, AVVCHeaderLength);
+            uint32_t NALUUnitLength = 0;
             
+            memcpy(&NALUUnitLength, dataPointer + bufferOffSet, AVVCHeaderLength);
+            // 从大端模式转换为系统端模式
+            NALUUnitLength = CFSwapInt32BigToHost(NALUUnitLength);
             //获取nalu
-            NSData *data = [[NSData alloc]initWithBytes:(dataPointer + bufferOffSet + AVVCHeaderLength) length:NALUnitLength];
+            NSData *data = [[NSData alloc]initWithBytes:(dataPointer + bufferOffSet + AVVCHeaderLength) length:NALUUnitLength];
             
             //将nalu数据写入到文件
             [encoder gotEncodedData:data isKeyFrame:keyFrame];
             
             //读取下一个NALU数据
-            bufferOffSet += AVVCHeaderLength + NALUnitLength;
+            bufferOffSet += AVVCHeaderLength + NALUUnitLength;
             
             
         }
     }
     
 }
+
 /*
  序列参数集SPS
  图像参数集合PPS
  
  编码所用的Profile、level、图像的宽和高、deblock录波器。。。。
  h264码流第一个NALU是 SPS & PPS
- 
  
  (1)第一位为禁位
  (2)第2-3位为参考级别
@@ -372,7 +382,6 @@ void didCompressH264(void *outputCallbackRefCon, void *sourceFrameRefCon, OSStat
     [fileHandele writeData:byteHeader];
     
     [fileHandele writeData:pps];
-    
     
 }
 
@@ -402,7 +411,7 @@ void didCompressH264(void *outputCallbackRefCon, void *sourceFrameRefCon, OSStat
     }
 }
 
-//结束VideoToolBox
+//结束VideoToolBox,停止编码
 -(void)endVideoToolBox
 {
     VTCompressionSessionCompleteFrames(cEncodeingSession, kCMTimeInvalid);
